4bb5e4d04cedda0c4346752dacc841bc969de61e
ColeMalinchock1 <malinchc@gmail.com>
2024-07-15 13:40:59 -0400

    Adding od node

---
+++ sUAV/obstacle_avoidance_node.py
@@ -0,0 +1,6 @@
+
+"""
+This node is used to read the depth from the Zed and lidar and is capable of detecting if an obstacle is obstructing its current path.
+It creates a new waypoint in the case of needing to route around an obstacle.
+"""
+
